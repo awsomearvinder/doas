2b85320df2cb40dbbcfae4a7253be7d2dceddde4
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2020-08-19 13:54:11 -0500

    Move options and identity detection to a seperate function in parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -10,7 +10,7 @@ mod tests;
 mod lexer;
 
 #[allow(dead_code)]
-pub fn parse_rules(contents: &str) -> Vec<Result<Rule, ParserError<&str>>> {
+pub fn parse_rules<'a>(contents: &'a str) -> Vec<Result<Rule, ParserError<'a>>> {
     let tokens = dbg!(lexer::get_tokens(contents).unwrap_or_else(|e| panic!("Got err {:?}", e)));
     let mut tokens = tokens.into_iter();
     let mut rules = vec![];
@@ -21,22 +21,10 @@ pub fn parse_rules(contents: &str) -> Vec<Result<Rule, ParserError<&str>>> {
         Some(token) => panic!("Expected rule in config got {:?} token", token),
         None => return rules,
     };
-    let mut rule = rule;
-    loop {
-        match tokens.next() {
-            Some(lexer::Token::NoPass) => rule = rule.no_pass(),
-            Some(lexer::Token::Persist) => rule = rule.persist(),
-            Some(lexer::Token::KeepEnv) => rule = rule.keep_env(),
-            Some(lexer::Token::SetEnv(m)) => rule = rule.set_env(m),
-            Some(lexer::Token::Ident(user_identity)) => {
-                rule = rule.identity_name(user_identity);
-                break;
-            }
-            Some(token) => panic!("Expected option or user identity got token {:?}", token),
-            None => panic!("Expected option or user identity, got nothing."),
-        }
-    }
+
+    let mut rule = get_options_and_identity(rule, &mut tokens).unwrap();
     eprintln!("{:?}", contents);
+
     'outer: loop {
         match tokens.next() {
             Some(lexer::Token::As) => match tokens.next() {
@@ -85,4 +73,24 @@ pub fn parse_rules(contents: &str) -> Vec<Result<Rule, ParserError<&str>>> {
         }
     }
     rules
+}
+
+pub fn get_options_and_identity<'a, T: Iterator<Item = lexer::Token<'a>>>(
+    mut builder: rules::RuleBuilder<'a>,
+    tokens: &mut T,
+) -> Result<rules::RuleBuilder<'a>, ParserError<'a>> {
+    loop {
+        match tokens.next() {
+            Some(lexer::Token::NoPass) => builder = builder.no_pass(),
+            Some(lexer::Token::Persist) => builder = builder.persist(),
+            Some(lexer::Token::KeepEnv) => builder = builder.keep_env(),
+            Some(lexer::Token::SetEnv(m)) => builder = builder.set_env(m),
+            Some(lexer::Token::Ident(user_identity)) => {
+                builder = builder.identity_name(user_identity);
+                return Ok(builder);
+            }
+            Some(token) => return Err(ParserError::ExpectedOptionOrIdentityGot(token)),
+            None => return Err(ParserError::ExpectedOptionOrIdentityGot(lexer::Token::EOL)),
+        }
+    }
 }
--- src/parser/parser_err.rs
+++ src/parser/parser_err.rs
@@ -1,53 +1,18 @@
-use std::collections::HashMap;
-
 #[derive(Debug, PartialEq, Eq)]
-pub enum ParserError<'a, I> {
-    NoTarget,
-    FoundTrailingAfterRule,
-    NoArgsLeft,
-    UnknownRule(String),
-    NoUser,
-    NoRule,
-    NoCmd,
-    NoCmdArgs,
-    UnmatchedOrNoSetEnvBracket,
-    NomError(I, nom::error::ErrorKind),
-    InvalidKeyValsInSetEnv(HashMap<&'a str, &'a str>),
+pub enum ParserError<'a> {
+    ExpectedOptionOrIdentityGot(super::lexer::Token<'a>),
 }
 
-impl<'a, I> nom::error::ParseError<I> for ParserError<'a, I> {
-    fn from_error_kind(input: I, kind: nom::error::ErrorKind) -> Self {
-        Self::NomError(input, kind)
-    }
-    fn append(_: I, _: nom::error::ErrorKind, other: Self) -> Self {
-        other
-    }
-}
-
-impl<'a, I> std::fmt::Display for ParserError<'a, I> {
+impl<'a> std::fmt::Display for ParserError<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::FoundTrailingAfterRule => write!(f, "Found trailing characters after the rule"),
-            Self::NoArgsLeft => write!(f, "No args are left."),
-            Self::NoUser => write!(f, "No user identity provided"),
-            Self::NoRule => write!(f, "No rule provided in config"),
-            Self::UnknownRule(e) => write!(f, "Encountered an Unknown rule {}", e),
-            Self::InvalidKeyValsInSetEnv(map) => write!(
-                f,
-                "unequal amount of keyvalue pairs in setenv arg {:#?}",
-                map
-            ),
-            Self::UnmatchedOrNoSetEnvBracket => {
-                write!(f, "You have an unmatched or no setenv bracket.")
+        Ok(match self {
+            Self::ExpectedOptionOrIdentityGot(token) => {
+                write!(
+                    f,
+                    "Expected an option to rule (e.g. persist, nopass, keepenv, setenv) or user identity got \"{}\".",
+                    token
+                );
             }
-            Self::NomError(_, _) => write!(
-                f,
-                "Got some internal nom error, consider sending a bug report! context: {}",
-                self
-            ),
-            Self::NoTarget => write!(f, "No provided target for rule after as keyword"),
-            Self::NoCmd => write!(f, "No provided command after cmd keyword"),
-            Self::NoCmdArgs => write!(f, "No provided command after args keyword"),
-        }
+        })
     }
 }
